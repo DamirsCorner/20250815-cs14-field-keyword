830d108588382805872124a323892dacbc6b99a7
Damir Arh <damir.arh@gmail.com>
2025-07-27 11:41:05 +0200

    Add project files.

---
+++ Cs14FieldKeyword/BeforeCs14Tests.cs
@@ -0,0 +1,62 @@
+﻿using Shouldly;
+
+namespace Cs14FieldKeyword;
+
+public class BeforeCs14Tests
+{
+    public class SampleClass
+    {
+        public int NonValidatedProperty { get; set; }
+
+        private int validatedPropertyValue;
+        public int ValidatedProperty
+        {
+            get { return validatedPropertyValue; }
+            set
+            {
+                validatedPropertyValue =
+                    value >= 0
+                        ? value
+                        : throw new ArgumentOutOfRangeException(
+                            nameof(value),
+                            "The value must be non-negative."
+                        );
+            }
+        }
+
+        public void SetBackingField(int value)
+        {
+            validatedPropertyValue = value;
+        }
+    }
+
+    [Test]
+    public void SettingNonValidatedPropertySucceeds()
+    {
+        SampleClass instance = new();
+
+        instance.NonValidatedProperty = -1;
+
+        instance.NonValidatedProperty.ShouldBe(-1);
+    }
+
+    [Test]
+    public void SettingValidatedPropertyThrows()
+    {
+        SampleClass instance = new();
+
+        Should.Throw<ArgumentOutOfRangeException>(() => instance.ValidatedProperty = -1);
+
+        instance.ValidatedProperty.ShouldBe(0);
+    }
+
+    [Test]
+    public void SettingValidatedPropertyWithoutValidationSucceeds()
+    {
+        SampleClass instance = new();
+
+        instance.SetBackingField(-1);
+
+        instance.ValidatedProperty.ShouldBe(-1);
+    }
+}
---
+++ Cs14FieldKeyword/WithCs14Tests.cs
@@ -0,0 +1,66 @@
+﻿using Shouldly;
+
+namespace Cs14FieldKeyword;
+
+public class WithCs14Tests
+{
+    public class SampleClass
+    {
+        public int ValidatedProperty
+        {
+            get;
+            set
+            {
+                field =
+                    value >= 0
+                        ? value
+                        : throw new ArgumentOutOfRangeException(
+                            nameof(value),
+                            "The value must be non-negative."
+                        );
+            }
+        }
+
+        private int field;
+        public int FieldProperty
+        {
+            get { return @field; }
+            set { this.field = value; }
+        }
+
+        public int GetFieldValue()
+        {
+            return field;
+        }
+    }
+
+    [Test]
+    public void SettingValidatedPropertyThrows()
+    {
+        SampleClass instance = new();
+
+        Should.Throw<ArgumentOutOfRangeException>(() => instance.ValidatedProperty = -1);
+
+        instance.ValidatedProperty.ShouldBe(0);
+    }
+
+    [Test]
+    public void SettingValidatedPropertyDoesNotSetDeclaredField()
+    {
+        SampleClass instance = new();
+
+        instance.ValidatedProperty = 1;
+
+        instance.GetFieldValue().ShouldBe(0);
+    }
+
+    [Test]
+    public void SettingFieldPropertySetsDeclaredField()
+    {
+        SampleClass instance = new();
+
+        instance.FieldProperty = 1;
+
+        instance.GetFieldValue().ShouldBe(1);
+    }
+}
